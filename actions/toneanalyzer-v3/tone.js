9eedae18f0bcb3629d1a894922883ecd114ad4ae
Ammar Dodin <ammar.dodin@ibm.com>
2017-09-26 16:58:21 -0400

    convert double quotes to single quotes

--- actions/toneanalyzer-v3/tone.js
+++ actions/toneanalyzer-v3/tone.js
@@ -1,24 +1,25 @@
-module.exports.main = main;
 const ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
 
 /**
- * @param {object} params - The parameters object
- * @param {string} params.username - The ToneAnalyzer service username
- * @param {string} params.password - The ToneAnalyzer service password
- * @param {string} [params.version_date = '2016-05-19'] - The ToneAnalyzer service version date
- * @param {string} params.text - The Text To Be Analyzed For Tone
- * @param {string} [params.tone = ['emotion','language','social']] - A list of tones for which the service to return its analysis of the input
- * @param {boolean} [params.sentences = true] - Indicates whether the service is to return an analysis of each individual
- * sentence in addition to its analysis of the full document
- * @param {boolean} [params.isHTML = true] - Indicates whether the text parameter provides HTML input
+ * @param {Object} params - The parameters object
+ * @param {String} [params.username] - The ToneAnalyzer service username
+ * @param {String} [params.password] - The ToneAnalyzer service password
+ * @param {Boolean} [params.use_unauthenticated] - Skip credential requirement
+ * @param {Object} [params.headers]
+ * @param {Boolean} [params.headers.X-Watson-Learning-Opt-Out=false] - Opt-out of data collection
+ * @param {String} params.version_date - The ToneAnalyzer service version date
+ * @param {String} params.text - The Text To Be Analyzed For Tone
+ * @param {String} [params.tone = ['emotion','language','social']] - A list of tones for which the
+ * service to return its analysis of the input
+ * @param {Boolean} [params.sentences = true] - Indicates whether the service is to
+ * return an analysis of each individual sentence in addition to its analysis of the full document
+ * @param {Boolean} [params.isHTML = true] - Indicates whether the text parameter provides HTML input
+ * 
+ * @return {Promise}
  */
 function main(params) {
-  return new Promise(((resolve, reject) => {
-    const service = new ToneAnalyzerV3({
-      username: params.username,
-      password: params.password,
-      version_date: params.version_date,
-    });
+  return new Promise((resolve, reject) => {
+    const service = new ToneAnalyzerV3(params);
 
     service.tone(params, (err, tone) => {
       if (err) {
@@ -27,5 +28,7 @@ function main(params) {
         resolve(tone);
       }
     });
-  }));
+  });
 }
+
+module.exports.main = main;
--- test/actions/toneanalyzer-v3/tone.test.js
+++ test/actions/toneanalyzer-v3/tone.test.js
@@ -1,129 +1,129 @@
-const assert = require("assert");
-const nock = require("nock");
-const action = require("../../../actions/toneanalyzer-v3/tone");
+const assert = require('assert');
+const nock = require('nock');
+const action = require('../../../actions/toneanalyzer-v3/tone');
 
-describe("[action] ToneAnalyzer", () => {
+describe('[action] ToneAnalyzer', () => {
   beforeEach(() => {
-    nock("https://gateway.watsonplatform.net/tone-analyzer")
-      .post("/api/v3/tone")
+    nock('https://gateway.watsonplatform.net/tone-analyzer')
+      .post('/api/v3/tone')
       .query({
-        version: "fake version date"
+        version: 'fake version date'
       })
       .reply(200, {
-        "fake-key": "fake-value"
+        'fake-key': 'fake-value'
       });
   });
 
-  it("should fail if params is undefined", () => {
+  it('should fail if params is undefined', () => {
     const params = undefined;
     return action
       .main(params)
       .then(() => {
-        assert.fail("Undefined parameters error was not found");
+        assert.fail('Undefined parameters error was not found');
       })
       .catch(err => {
         assert(
           err.message ===
-            "Argument error: username and password are required unless use_unauthenticated is set"
+            'Argument error: username and password are required unless use_unauthenticated is set'
         );
       });
   });
 
-  it("should fail if params is null", () => {
+  it('should fail if params is null', () => {
     const params = null;
     return action
       .main(params)
       .then(() => {
-        assert.fail("Null parameters error was not found");
+        assert.fail('Null parameters error was not found');
       })
       .catch(err => {
         assert(
           err.message ===
-            "Argument error: username and password are required unless use_unauthenticated is set"
+            'Argument error: username and password are required unless use_unauthenticated is set'
         );
       });
   });
 
-  it("should fail if username is not provided", () => {
+  it('should fail if username is not provided', () => {
     const params = {
-      password: "fake password",
-      text: "fake text",
-      version_date: "fake version date"
+      password: 'fake password',
+      text: 'fake text',
+      version_date: 'fake version date'
     };
     return action
       .main(params)
       .then(() => {
-        assert.fail("Missing username error was not found");
+        assert.fail('Missing username error was not found');
       })
       .catch(err => {
         assert(
           err.message ===
-            "Argument error: username and password are required unless use_unauthenticated is set"
+            'Argument error: username and password are required unless use_unauthenticated is set'
         );
       });
   });
 
-  it("should fail if password is not provided", () => {
+  it('should fail if password is not provided', () => {
     const params = {
-      username: "fake username",
-      text: "fake text",
-      version_date: "fake version date"
+      username: 'fake username',
+      text: 'fake text',
+      version_date: 'fake version date'
     };
     return action
       .main(params)
       .then(() => {
-        assert.fail("Missing password error was not found");
+        assert.fail('Missing password error was not found');
       })
       .catch(err => {
         assert(
           err.message ===
-            "Argument error: username and password are required unless use_unauthenticated is set"
+            'Argument error: username and password are required unless use_unauthenticated is set'
         );
       });
   });
 
-  it("should fail if version_date is not provided", () => {
+  it('should fail if version_date is not provided', () => {
     const params = {
-      username: "fake username",
-      password: "fake password",
-      text: "fake text"
+      username: 'fake username',
+      password: 'fake password',
+      text: 'fake text'
     };
     return action
       .main(params)
       .then(() => {
-        assert.fail("Missing version_date error was not found");
+        assert.fail('Missing version_date error was not found');
       })
       .catch(err => {
         assert(
           err.message ===
-            "Argument error: version_date was not specified, use 2016-05-19"
+            'Argument error: version_date was not specified, use 2016-05-19'
         );
       });
   });
 
-  it("should fail if text is not provided", () => {
+  it('should fail if text is not provided', () => {
     const params = {
-      username: "fake username",
-      password: "fake password",
-      version_date: "fake version date"
+      username: 'fake username',
+      password: 'fake password',
+      version_date: 'fake version date'
     };
 
     return action
       .main(params)
       .then(() => {
-        assert.fail("Missing text error was not found");
+        assert.fail('Missing text error was not found');
       })
       .catch(err => {
-        assert(err.message === "Missing required parameters: text");
+        assert(err.message === 'Missing required parameters: text');
       });
   });
 
-  it("it should pass if all required parameters are provided", () => {
+  it('it should pass if all required parameters are provided', () => {
     const params = {
-      username: "fake username",
-      password: "fake password",
-      version_date: "fake version date",
-      text: "fake text"
+      username: 'fake username',
+      password: 'fake password',
+      version_date: 'fake version date',
+      text: 'fake text'
     };
 
     return action
@@ -134,10 +134,10 @@ describe("[action] ToneAnalyzer", () => {
       .catch(assert.ifError);
   });
 
-  it("it should pass if username and password are omitted but use_unauthenticated is set", () => {
+  it('it should pass if username and password are omitted but use_unauthenticated is set', () => {
     const params = {
-      text: "fake text",
-      version_date: "fake version date",
+      text: 'fake text',
+      version_date: 'fake version date',
       use_unauthenticated: true
     };
 

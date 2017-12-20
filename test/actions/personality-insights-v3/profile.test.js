7d75477c70c479f0aa73a0e00341d6d440e01c32
Ammar Dodin <ammar.dodin@ibm.com>
2017-12-19 21:24:39 -0500

    :green_heart: add tests for personality-insights-v3

--- test/actions/personality-insights-v3/profile.test.js
+++ test/actions/personality-insights-v3/profile.test.js
@@ -1,154 +1,92 @@
 const assert = require('assert');
 const nock = require('nock');
-const action = require('../../../actions/personality-insights-v3/profile');
-const serviceName = 'PERSONALITY INSIGHTS V3';
+const extend = require('extend');
+const path = require('path');
+const fs = require('fs');
+const omit = require('object.omit');
+const openwhisk = require('openwhisk');
+const auth = require('../../resources/auth');
+const { adapt, negativeHandler } = require('../../resources/test-helper');
+let profile = require('../../../actions/personality-insights-v3/profile');
 
-describe('[action] Personality Insights', () => {
-  beforeEach(() => {
-    nock('https://gateway.watsonplatform.net/personality-insights')
-      .post('/api/v3/profile')
-      .query({
-        version: 'fake version date',
-      })
-      .reply(200, {
-        'fake-key': 'fake-value',
-      });
-  });
+let ow;
+let credentials;
+let payload = {
+  content: fs
+    .readFileSync(path.join(__dirname, '/../../resources/personality_insights_content.txt'))
+    .toString(),
+  content_type: 'text/plain'
+};
 
-  it('should fail if params is undefined', () => {
-    const params = undefined;
-    return action
-      .test(params)
-      .then(() => {
-        assert.fail('Undefined parameters error was not found');
-      })
-      .catch((err) => {
-        assert(err ===
-            `Argument error: username and password are required for ${serviceName} unless use_unauthenticated is set`);
-      });
-  });
-
-  it('should fail if params is null', () => {
-    const params = null;
-    return action
-      .test(params)
-      .then(() => {
-        assert.fail('Null parameters error was not found');
-      })
-      .catch((err) => {
-        assert(err ===
-          `Argument error: username and password are required for ${serviceName} unless use_unauthenticated is set`);
-      });
-  });
-
-  it('should fail if username is not provided', () => {
-    const params = {
-      password: 'fake password',
-      version_date: 'fake version date',
-      text: 'fake text',
+before(() => {
+  if (process.env.TEST_OPENWHISK) {
+    ow = openwhisk(auth.ow);
+    profile = adapt(profile, 'personality-insights-v3/profile', ow);
+    credentials = auth.personality_insights.v3;
+  } else {
+    credentials = {
+      username: 'username',
+      password: 'password',
+      version_date: 'version-date'
     };
-    return action
+    beforeEach(() => {
+      nock('https://gateway.watsonplatform.net/personality-insights')
+        .post('/api/v3/profile')
+        .query({
+          version: credentials.version_date
+        })
+        .reply(200, {});
+    });
+  }
+  payload = extend({}, payload, credentials);
+});
+
+describe('profile', () => {
+  it('should fail if credentials are missing', () => {
+    const params = omit(payload, ['username', 'password']);
+    return profile
       .test(params)
       .then(() => {
-        assert.fail('Missing username error was not found');
+        assert.fail('No failure on missing credentials');
       })
-      .catch((err) => {
-        assert(err ===
-          `Argument error: username and password are required for ${serviceName} unless use_unauthenticated is set`);
+      .catch(err => negativeHandler(err));
+  });
+  it('should fail if version_date is missing', () => {
+    const params = omit(payload, ['version_date']);
+    return profile
+      .test(params)
+      .then(() => {
+        assert.fail('No failure on missing version_date');
+      })
+      .catch(err => negativeHandler(err));
+  });
+  it('should fail if content is missing', () => {
+    const params = omit(payload, ['content']);
+    return profile
+      .test(params)
+      .then(() => {
+        assert.fail('No failure on missing content');
+      })
+      .catch(err => negativeHandler(err));
+  });
+  it('should fail if content_type is missing', () => {
+    const params = omit(payload, ['content_type']);
+    return profile
+      .test(params)
+      .then(() => {
+        assert.fail('No failure on missing content_type');
+      })
+      .catch(err => negativeHandler(err));
+  });
+  it('should generate a valid payload', () => {
+    const params = payload;
+    return profile
+      .test(params)
+      .then(() => {
+        assert.ok(true);
+      })
+      .catch(() => {
+        assert.fail('Failure on valid payload');
       });
-  });
-
-  it('should fail if password is not provided', () => {
-    const params = {
-      username: 'fake username',
-      version_date: 'fake version date',
-      text: 'fake text',
-    };
-    return action
-      .test(params)
-      .then(() => {
-        assert.fail('Missing password error was not found');
-      })
-      .catch((err) => {
-        assert(err ===
-          `Argument error: username and password are required for ${serviceName} unless use_unauthenticated is set`);
-      });
-  });
-
-  it('should fail if version_date is not provided', () => {
-    const params = {
-      username: 'fake username',
-      password: 'fake password',
-      text: 'fake text',
-    };
-    return action
-      .test(params)
-      .then(() => {
-        assert.fail('Missing version_date error was not found');
-      })
-      .catch((err) => {
-        assert(err ===
-            'Argument error: version_date was not specified');
-      });
-  });
-
-  it('should fail if neither text nor content_items is provided', () => {
-    const params = {
-      username: 'fake username',
-      password: 'fake password',
-      version_date: 'fake version date',
-    };
-    return action
-      .test(params)
-      .then(() => {
-        assert.fail('Missing text and content_items error was not found');
-      })
-      .catch((err) => {
-        assert(err === 'Missing required parameters: text or content_items');
-      });
-  });
-
-  it('should pass if all required parameters are provided', () => {
-    const params = {
-      username: 'fake username',
-      password: 'fake password',
-      text: 'fake text',
-      version_date: 'fake version date',
-    };
-    return action
-      .test(params)
-      .then(() => {
-        assert(true);
-      })
-      .catch(assert.ifError);
-  });
-
-  it('should pass if all required parameters are provided', () => {
-    const params = {
-      username: 'fake username',
-      password: 'fake password',
-      version_date: 'fake version date',
-      content_items: { text: 'fake text' },
-    };
-    return action
-      .test(params)
-      .then(() => {
-        assert(true);
-      })
-      .catch(assert.ifError);
-  });
-
-  it('it should pass if username and password are omitted but use_unauthenticated is set', () => {
-    const params = {
-      version_date: 'fake version date',
-      text: 'fake text',
-      use_unauthenticated: true,
-    };
-    return action
-      .test(params)
-      .then(() => {
-        assert(true);
-      })
-      .catch(assert.ifError);
   });
 });

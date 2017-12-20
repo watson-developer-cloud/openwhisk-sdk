de2426021bfdc88ecd4f57753e2fb3862619c9bd
Ammar Dodin <ammar.dodin@ibm.com>
2017-12-19 21:23:10 -0500

    :green_heart: add tests for langauge-translator-v2

---
+++ test/actions/language-translator-v2/list-models.test.js
@@ -0,0 +1,54 @@
+const assert = require('assert');
+const nock = require('nock');
+const extend = require('extend');
+const omit = require('object.omit');
+const openwhisk = require('openwhisk');
+const auth = require('../../resources/auth');
+const { adapt, negativeHandler } = require('../../resources/test-helper');
+let listModels = require('../../../actions/language-translator-v2/list-models');
+
+let ow;
+let credentials;
+let payload = {};
+
+before(() => {
+  if (process.env.TEST_OPENWHISK) {
+    ow = openwhisk(auth.ow);
+    listModels = adapt(listModels, 'language-translator-v2/list-models', ow);
+    credentials = auth.language_translator;
+  } else {
+    credentials = {
+      username: 'username',
+      password: 'password'
+    };
+    beforeEach(() => {
+      nock('https://gateway.watsonplatform.net/language-translator')
+        .get('/api/v2/models')
+        .reply(200, {});
+    });
+  }
+  payload = extend({}, payload, credentials);
+});
+
+describe('list-models', () => {
+  it('should fail if credentials are missing', () => {
+    const params = omit(payload, ['username', 'password']);
+    return listModels
+      .test(params)
+      .then(() => {
+        assert.fail('No failure on missing credentials');
+      })
+      .catch(err => negativeHandler(err));
+  });
+  it('should generate a valid payload', () => {
+    const params = payload;
+    return listModels
+      .test(params)
+      .then(() => {
+        assert.ok(true);
+      })
+      .catch(() => {
+        assert.fail('Failure on valid payload');
+      });
+  });
+});
---
+++ test/actions/language-translator-v2/translate.test.js
@@ -0,0 +1,85 @@
+const assert = require('assert');
+const nock = require('nock');
+const extend = require('extend');
+const omit = require('object.omit');
+const openwhisk = require('openwhisk');
+const auth = require('../../resources/auth');
+const { adapt, negativeHandler } = require('../../resources/test-helper');
+let translate = require('../../../actions/language-translator-v2/translate');
+
+let ow;
+let credentials;
+let payload = {
+  text: 'I love my son.',
+  source: 'en',
+  target: 'fr'
+};
+
+before(() => {
+  if (process.env.TEST_OPENWHISK) {
+    ow = openwhisk(auth.ow);
+    translate = adapt(translate, 'language-translator-v2/translate', ow);
+    credentials = auth.language_translator;
+  } else {
+    credentials = {
+      username: 'username',
+      password: 'password'
+    };
+    beforeEach(() => {
+      nock('https://gateway.watsonplatform.net/language-translator')
+        .post('/api/v2/translate')
+        .reply(200, {});
+    });
+  }
+  payload = extend({}, payload, credentials);
+});
+
+describe('translate', () => {
+  it('should fail if credentials are missing', () => {
+    const params = omit(payload, ['username', 'password']);
+    return translate
+      .test(params)
+      .then(() => {
+        assert.fail('No failure on missing credentials');
+      })
+      .catch(err => negativeHandler(err));
+  });
+  it('should fail if text is missing', () => {
+    const params = omit(payload, ['text']);
+    return translate
+      .test(params)
+      .then(() => {
+        assert.fail('No failure on missing text');
+      })
+      .catch(err => negativeHandler(err));
+  });
+  it('should fail if target is missing', () => {
+    const params = omit(payload, ['target']);
+    return translate
+      .test(params)
+      .then(() => {
+        assert.fail('No failure on missing target');
+      })
+      .catch(err => negativeHandler(err));
+  });
+  it('should fail if source is missing', () => {
+    const params = omit(payload, ['source']);
+    return translate
+      .test(params)
+      .then(() => {
+        assert.fail('No failure on missing source');
+      })
+      .catch(err => negativeHandler(err));
+  });
+  it('should generate a valid payload', () => {
+    const params = payload;
+    return translate
+      .test(params)
+      .then(() => {
+        assert.ok(true);
+      })
+      .catch(() => {
+        assert.fail('Failure on valid payload');
+      });
+  });
+});

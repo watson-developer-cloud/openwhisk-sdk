1afe0e33c7678563b678b4d87b3d7dbf67b5c0a1
Ammar Dodin <ammar.dodin@ibm.com>
2017-12-19 21:24:08 -0500

    :green_heart: add tests for natural-language-understanding-v1

---
+++ test/actions/natural-language-understanding-v1/delete-model.test.js
@@ -0,0 +1,71 @@
+const assert = require('assert');
+const nock = require('nock');
+const extend = require('extend');
+const omit = require('object.omit');
+const openwhisk = require('openwhisk');
+const auth = require('../../resources/auth');
+const { adapt, negativeHandler } = require('../../resources/test-helper');
+let deleteModel = require('../../../actions/natural-language-understanding-v1/delete-model');
+
+let ow;
+let credentials;
+let payload = {
+  model_id: 'fake-model'
+};
+
+before(() => {
+  if (process.env.TEST_OPENWHISK) {
+    ow = openwhisk(auth.ow);
+    deleteModel = adapt(
+      deleteModel,
+      'natural-language-understanding-v1/delete-model',
+      ow
+    );
+    credentials = auth.natural_language_understanding;
+  } else {
+    credentials = {
+      username: 'username',
+      password: 'password',
+      version_date: 'version-date'
+    };
+    beforeEach(() => {
+      nock('https://gateway.watsonplatform.net/natural-language-understanding')
+        .delete(`/api/v1/models/${payload.model_id}`)
+        .query({
+          version: credentials.version_date
+        })
+        .reply(200, {});
+    });
+  }
+  payload = extend({}, payload, credentials);
+});
+
+describe('delete-model', () => {
+  it('should fail if credentials are missing', () => {
+    const params = omit(payload, ['username', 'password']);
+    return deleteModel
+      .test(params)
+      .then(() => {
+        assert.fail('No failure on missing credentials');
+      })
+      .catch(err => negativeHandler(err));
+  });
+  it('should fail if version_date is missing', () => {
+    const params = omit(payload, ['version_date']);
+    return deleteModel
+      .test(params)
+      .then(() => {
+        assert.fail('No failure on missing version_date');
+      })
+      .catch(err => negativeHandler(err));
+  });
+  it('should fail if model_id is missing', () => {
+    const params = omit(payload, ['model_id']);
+    return deleteModel
+      .test(params)
+      .then(() => {
+        assert.fail('No failure on missing model_id');
+      })
+      .catch(err => negativeHandler(err));
+  });
+});
---
+++ test/actions/natural-language-understanding-v1/list-models.test.js
@@ -0,0 +1,71 @@
+const assert = require('assert');
+const nock = require('nock');
+const extend = require('extend');
+const omit = require('object.omit');
+const openwhisk = require('openwhisk');
+const auth = require('../../resources/auth');
+const { adapt, negativeHandler } = require('../../resources/test-helper');
+let listModels = require('../../../actions/natural-language-understanding-v1/list-models');
+
+let ow;
+let credentials;
+let payload = {};
+
+before(() => {
+  if (process.env.TEST_OPENWHISK) {
+    ow = openwhisk(auth.ow);
+    listModels = adapt(
+      listModels,
+      'natural-language-understanding-v1/list-models',
+      ow
+    );
+    credentials = auth.natural_language_understanding;
+  } else {
+    credentials = {
+      username: 'username',
+      password: 'password',
+      version_date: 'version-date'
+    };
+    beforeEach(() => {
+      nock('https://gateway.watsonplatform.net/natural-language-understanding')
+        .get('/api/v1/models')
+        .query({
+          version: credentials.version_date
+        })
+        .reply(200, {});
+    });
+  }
+  payload = extend({}, payload, credentials);
+});
+
+describe('list-models', () => {
+  it('should fail if credentials are missing', () => {
+    const params = omit(payload, ['username', 'password']);
+    return listModels
+      .test(params)
+      .then(() => {
+        assert.fail('No failure on missing credentials');
+      })
+      .catch(err => negativeHandler(err));
+  });
+  it('should fail if version_date is missing', () => {
+    const params = omit(payload, ['version_date']);
+    return listModels
+      .test(params)
+      .then(() => {
+        assert.fail('No failure on missing version_date');
+      })
+      .catch(err => negativeHandler(err));
+  });
+  it('should generate a valid payload', () => {
+    const params = payload;
+    return listModels
+      .test(params)
+      .then(() => {
+        assert.ok(true);
+      })
+      .catch(() => {
+        assert.fail('Failure on valid payload');
+      });
+  });
+});

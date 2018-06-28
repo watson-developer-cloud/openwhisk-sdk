c194d191a79dad2e1e94eeeaea2addf6c84dc6a9
Logan Patino <loganpatino10@gmail.com>
2018-06-28 17:03:44 -0400

    style: Fix linting errors

--- packages/language-translator-v2/actions/list-identifiable-languages.js
+++ packages/language-translator-v2/actions/list-identifiable-languages.js
@@ -21,7 +21,7 @@ const extend = require('extend');
  * List identifiable languages.
  *
  * Lists the languages that the service can identify. Returns the language code (for example, `en` for English or `es` for Spanish) and name of each language.
- * 
+ *
  * @deprecated Language Translator v3 is now available. The v2 Language Translator API will no longer be available after July 31, 2018. To take advantage of the latest service enhancements, migrate to the v3 API. View the [Migrating to Language Translator v3](https://console.bluemix.net/docs/services/language-translator/migrating.html) page for more information.
  *
  * @param {Object} params - The parameters to send to the service.

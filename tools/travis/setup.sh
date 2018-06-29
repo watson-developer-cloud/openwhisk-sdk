cada2f0ec4bad3553f012eabc3c63dd82ccee888
Belinda Vennam <beemarie88@gmail.com>
2018-06-29 16:07:08 -0500

    remove duplicate scancode

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -5,17 +5,9 @@ HOMEDIR="$SCRIPTDIR/../../../"
 ROOTDIR="$SCRIPTDIR/../.."
 UTILDIR="$ROOTDIR/../incubator-openwhisk-utilities"
 
-# jshint support
-sudo apt-get -y install nodejs npm
-sudo npm install -g jshint
-
 # clone utilties repo. in order to run scanCode.py
 cd $HOMEDIR
 git clone https://github.com/apache/incubator-openwhisk-utilities.git
-
-cd $UTILDIR
-scancode/scanCode.py $ROOT_DIR
-
 
 # shallow clone OpenWhisk repo.
 git clone --depth 1 https://github.com/apache/incubator-openwhisk.git openwhisk
